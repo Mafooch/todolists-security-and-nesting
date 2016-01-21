6ae26358e15a5306fd392ada4bf7d53aa5d9e06d
Mafooch <joshuamaffuccio@gmail.com>
2016-01-21 17:34:36 -0500

    Add authentication helper methods to application_controller and update application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_login
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :logged_in?, :current_user
+  # these methods were already available to all subcontrollers.
+  # this just gives access to the views
+
+  def logged_in?
+    session[:user_id]
+  end
+
+  def current_user
+    @current_user ||= User.find session[:user_id]
+  end
+
+  def ensure_login
+    redirect_to login_path unless logged_in?
+  end
 end
